4724a33a89b83c493cb49bf86792724d2cd81afe
Sexettin <72075007+sexettin78@users.noreply.github.com>
2021-07-30 23:23:05 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,96 @@
+const qrcode = require('qrcode-terminal');
+
+const { Client } = require('whatsapp-web.js');
+const client = new Client();
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.initialize();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.on('message', message => {
+	if(message.body === 'test') {
+		client.sendMessage(message.from, 'program aktif ve çalışıyor');
+	}
+});
+
+
+
+
+client.on('message', message => {
+	if(message.body === 'Kanka') {
+		client.sendMessage(message.from, 'Efendim');
+	}
+});
+
+
+
+client.on('message', message => {
+	if(message.body === 'İyi geceler') {
+		message.reply('Sana da iyi geceler');
+	}
+});
+
+client.on('message', message => {
+	if(message.body === 'Günaydın') {
+		message.reply('Sana da günaydın');
+	}
+});
+
+
+client.on('message', message => {
+	if(message.body === 'Furkan') {
+		client.sendMessage(message.from, 'Efendim');
+	}
+});
+
+
+
+client.on('message', message => {
+	if(message.body === 'Sa') {
+		message.reply('aleykümselam');
+	}
+});
+
+
+
+client.on('message', message => {
+	if(message.body === 'iyi geceler') {
+		message.reply('İyi geceler');
+	}
+});
+
+
+client.on('message', message => {
+	if(message.body === 'Nasılsın') {
+		message.reply('iyiyim sen nasılsın?');
+	}
+});
+
+
+
