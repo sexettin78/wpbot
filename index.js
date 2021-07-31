30bedff8457674edd9f94197c6f105c878c9da45
Sexettin <72075007+sexettin78@users.noreply.github.com>
2021-08-01 00:10:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const qrcode = require('qrcode-terminal');
 
-const { Client } = require('whatsapp-web.js');
+const {Client} = require('whatsapp-web.js');
 const client = new Client();
 
 client.on('qr', qr => {
